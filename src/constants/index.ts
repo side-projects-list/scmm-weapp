38b04032c1e89f305cd35d7d64e4f0b1af3b0ae5
wnor543 <wnor@live.cn>
2022-11-29 07:22:01 +0800

    chore: 新增基础常量

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,15 @@
 /** 首页路径 */
-export const HOME_PAGE_PATH = '/pages/index/indexindex'
+export const HOME_PAGE_PATH = '/pages/index/index/index'
+
+/** 底部TAbs页面路径 */
+export const TABS_PAGE = [HOME_PAGE_PATH, '/pages/order/index/index', '/pages/mine/index/index']
+
+/** 主色 */
+export const PRIMARY_COLOR = '#385AFF'
+export const ERROR_COLOR = '#eb483f'
+
+/** 登录验证码 */
+export const LOGIN_CAPTCHA_CODE = '666666'
 
 /** 默认头像 */
 export const DEFAULT_AVATAR = 'https://cdn.v2ex.com/gravatar/?d=identicon'
