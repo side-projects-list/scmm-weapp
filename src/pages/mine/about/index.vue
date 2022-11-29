8d47733ca98c4a3c1946fa1fdb1cdf25a1bc421e
wnor543 <wnor@live.cn>
2022-11-29 09:38:46 +0800

    feat: 新增关于我们页面

--- src/pages/mine/about/index.vue
+++ src/pages/mine/about/index.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts" name="about-index">
 // 关于我们页面
+
+const { handlePulldownRefresh } = useHooks()
+onPullDownRefresh(handlePulldownRefresh)
 </script>
 
 <template>
-  <view class="about-index">
-    about-index works!
+  <view class="about-index px-32rpx text-justify leading-loose">
+    <view class="text-center font-600 text-36rpx mb-32rpx">
+      SCMM - 简易汽车保养商城
+    </view>
+    <view class="indent-2em">
+      关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我。
+    </view>
+
+    <view class="indent-2em">
+      关于我们关于我们关于我们关于我于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我。
+    </view>
   </view>
 </template>
 
